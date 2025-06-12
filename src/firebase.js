e3a82a5be0292b5a361fe531ac43573d2d029e8c
Fabian <fbobllar@gmail.com>
2025-06-11 21:25:14 -0400

    back-end

--- src/firebase.js
+++ src/firebase.js
@@ -5,16 +5,18 @@ import { getStorage } from 'firebase/storage'; // <-- Import Storage
 
 // Your web app's Firebase configuration (REPLACE with your actual config)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "aviacion360-7a463", // <-- Your Project ID
-  storageBucket: "YOUR_STORAGE_BUCKET", // <-- Your Storage Bucket
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyD4IUefTEBmTPUHxqzOddnKZm4sGGbWGEA",
+  authDomain: "aviacion360-7a463.firebaseapp.com",
+  projectId: "aviacion360-7a463",
+  storageBucket: "aviacion360-7a463.firebasestorage.app",
+  messagingSenderId: "972139427481",
+  appId: "1:972139427481:web:4edb85c5e677a49fabaa08",
+  measurementId: "G-2ZCPNE6WRJ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
 
 // Get a reference to the services
 const db = getFirestore(app); // <-- Get Firestore instance
